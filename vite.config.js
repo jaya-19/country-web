edb911aaabe03f0df4f92a2a336757de313b821e
jaya-19 <jaya.stokeinfotech@gmail.com>
2024-10-23 12:05:10 +0530

    fourth commit

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173, // Change the port here
+  build: {
+    outDir: 'dist', // This is the default output directory for Vite
   },
-  base: '/'
-})
+  base: process.env.NODE_ENV === 'production' ? '/your-app/' : '/',
+});
+
